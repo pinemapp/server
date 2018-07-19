9846aa2ecdb1f60058726a0c7d23c58253ada198
PrinceNorin <norin.hor@gmail.com>
2018-07-19 21:27:58 +0700

    struct dirty binding utils

--- utils/validators/validator.go
+++ utils/validators/validator.go
@@ -2,6 +2,8 @@ package validators
 
 import (
 	"fmt"
+	"reflect"
+	"time"
 
 	"github.com/pinem/server/utils/messages"
 	validator "gopkg.in/go-playground/validator.v9"
@@ -29,4 +31,53 @@ func Validate(f interface{}, msg *messages.Messages) error {
 		return err
 	}
 	return nil
+}
+
+func Bind(model interface{}, f interface{}) {
+	modelIndirect := reflect.Indirect(reflect.ValueOf(model))
+	formElem := reflect.ValueOf(f).Elem()
+	tForm := formElem.Type()
+	for i := 0; i < formElem.NumField(); i++ {
+		tag := tForm.Field(i).Tag
+		if tag.Get("omit") != "true" {
+			modelField := modelIndirect.FieldByName(tForm.Field(i).Name)
+			if modelField.IsValid() {
+				formField := formElem.Field(i)
+				if formField.IsValid() {
+					if isDirty(modelField, formField) {
+						modelField.Set(formField)
+					}
+				}
+			}
+		}
+	}
+}
+
+func isDirty(f, ff reflect.Value) bool {
+	switch f.Kind() {
+	case reflect.String:
+		return f.String() != ff.String()
+	case reflect.Bool:
+		return f.Bool() != ff.Bool()
+	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+		return f.Int() != ff.Int()
+	case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64, reflect.Uintptr:
+		return f.Uint() != ff.Uint()
+	case reflect.Float32, reflect.Float64:
+		return f.Float() != f.Float()
+	case reflect.Interface, reflect.Ptr:
+		if ff.IsNil() {
+			return false
+		}
+		if f.IsNil() && !ff.IsNil() {
+			return true
+		}
+		return isDirty(f.Elem(), ff.Elem())
+	case reflect.Struct:
+		if t, ok := f.Interface().(time.Time); ok {
+			tt := ff.Interface().(time.Time)
+			return !t.Equal(tt)
+		}
+	}
+	return false
 }
